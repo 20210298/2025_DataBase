ba7ae4d334a71c308ac350cffd3033f8afb941da
zooz13 <zo_o13@naver.com>
2025-06-02 14:41:21 +0900

    ✨ Join 기능 추가

--- src/main/kotlin/kr/ac/kumoh/s20210298/mysql_robot/Robot.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210298/mysql_robot/Robot.kt
@@ -15,3 +15,25 @@ data class Robot(
     @Column(name = "class") // 데이터베이스에서 사용한 이름과 다를 때 선언
     var robotClass: String,
 )
+
+@Entity
+data class Weapon(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    val name: String,
+    val type: String,
+    val damage: Int,
+    val ability: String,
+)
+
+@Entity
+data class Equipment(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    var mechanicId: Int,    // DB에선 snake_case로 썼지만, 여기전 상관없음
+    var weaponId: Int,
+    val location: String,
+    val quantity: Int
+)
--- src/main/kotlin/kr/ac/kumoh/s20210298/mysql_robot/RobotService.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210298/mysql_robot/RobotService.kt
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Service
 @Service
 class RobotService(val repository: RobotRepository) {
     fun getAllRobots() = repository.findAll()   // 이미 findAll()이 JpaRepository에 구현되어 있음
+
+    fun getRobotWithWeapons(): List<EquipmentDto> {
+        return repository.findAllEquipment()
+    }
 }
